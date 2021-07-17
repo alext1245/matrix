d91a34460ee232f453234ad468bfaa6b0bba8144
Alex <slizxyr@gmail.com>
2021-07-17 19:35:45 +0300

    sec commit

--- matrixsd.py
+++ matrixsd.py
@@ -5,7 +5,6 @@ def summ_of_matrix(A, B):
             summ[i][j] = A[i][j] + B[i][j]
     return summ
 
-
 def composition_of_matrix(A,B):
     comp = [[0 for x in range(0, len(A[0]))] for x in range(len(A))]
     for i in range(0, len(A)):
@@ -14,6 +13,16 @@ def composition_of_matrix(A,B):
                 comp[i][j] += A[i][x]*B[x][j]
     return comp
 
+def subtraction_of_matrix(A,B):
+    sub = [[0 for x in range(0, len(A[0]))] for x in range(len(A))]
+    for i in range(0, len(A)):
+        for j in range(0, len(A[0])):
+            sub[i][j] = A[i][j] - B[i][j]
+    return sub
 
-
-
+def transpose(matrix):
+    tpmatrix = [[0 for x in range(0, len(matrix[0]))] for x in range(len(matrix))]
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            tpmatrix[i][j] = matrix[j][i]
+    return tpmatrix
