fd2a124a82e697e647ea8d21139dad44260ce39c
alext1245 <slizxyr@gmail.com>
2021-07-19 16:25:02 +0300

    rows and columns

--- classes.py
+++ classes.py
@@ -9,31 +9,31 @@ class Matrix:
         return len(self.matrix[0])
 
     def transpose(self):
-        tpmatrix = [[0 for x in range(0, len(self.matrix[0]))] for x in range(len(self.matrix))]
-        for i in range(0, len(self.matrix)):
-            for j in range(0, len(self.matrix[0])):
+        tpmatrix = [[0 for x in range(0, self.columns_count())] for x in range(self.rows_count())]
+        for i in range(0, self.rows_count()):
+            for j in range(0, self.columns_count()):
                 tpmatrix[i][j] = self.matrix[j][i]
         return tpmatrix
 
 
     def __add__(self, other):
-        summ = [[0 for x in range(0, len(self.matrix[0]))] for x in range(len(self.matrix))]
-        for i in range(0, len(self.matrix)):
-            for j in range(0, len(self.matrix[0])):
+        summ = [[0 for x in range(0, self.columns_count())] for x in range(self.rows_count())]
+        for i in range(0, self.rows_count()):
+            for j in range(0, self.columns_count()):
                 summ[i][j] = self.matrix[i][j] + other.matrix[i][j]
         return summ
 
     def __mul__(self, other):
-        comp = [[0 for x in range(0, len(self.matrix[0]))] for x in range(len(self.matrix))]
-        for i in range(0, len(self.matrix)):
-            for j in range(0, len(self.matrix[0])):
-                for x in range(0, len(self.matrix[0])):
+        comp = [[0 for x in range(0, self.columns_count())] for x in range(self.rows_count())]
+        for i in range(0, self.rows_count()):
+            for j in range(0, self.columns_count()):
+                for x in range(0, self.columns_count()):
                     comp[i][j] += self.matrix[i][x] * other.matrix[x][j]
         return comp
 
     def __sub__(self, other):
-        sub = [[0 for x in range(0, len(self.matrix[0]))] for x in range(len(self.matrix))]
-        for i in range(0, len(self.matrix)):
-            for j in range(0, len(self.matrix[0])):
+        sub = [[0 for x in range(0, self.columns_count())] for x in range(self.rows_count())]
+        for i in range(0, self.rows_count()):
+            for j in range(0, self.columns_count()):
                 sub[i][j] = self.matrix[i][j] - other.matrix[i][j]
         return sub
